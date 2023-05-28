7c0a3697e706ed4aa85aaeb57aeb4b8c0efe5dc7
seungiminRyu <fbtmdals011@naver.com>
2023-05-28 17:52:08 +0900

    Add: 카운터 앱 버튼, 로직 추가

--- src/routes/Counter.js
+++ src/routes/Counter.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 function Counter(props) {
+    const [value, setValue] = useState(0);
+
+    const onPlus = () => setValue((prev) => prev + 1);
+    const onMinus = () => setValue((prev) => prev - 1);
+
     return (
         <div>
             <h2 data-testid="title">카운터</h2>
+            <p data-testid="value">{value}</p>
+            <button data-testid="minus-btn" onClick={onMinus}>
+                -
+            </button>
+            <button data-testid="plus-btn" onClick={onPlus}>
+                +
+            </button>
         </div>
     );
 }
